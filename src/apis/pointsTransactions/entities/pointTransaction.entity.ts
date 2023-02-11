b2b8d7ec0e591520e992b50e7693de1ab3027e4c
nwd0907 <nwd0907@newbizstart.com>
2023-02-11 14:54:04 +0900

    first commit

--- src/apis/pointsTransactions/entities/pointTransaction.entity.ts
+++ src/apis/pointsTransactions/entities/pointTransaction.entity.ts
@@ -34,7 +34,7 @@ export class PointTransaction {
 
   @Column({ type: 'enum', enum: POINT_TRANSACTION_STATUS_ENUM })
   @Field(() => POINT_TRANSACTION_STATUS_ENUM)
-  status: string;
+  status: POINT_TRANSACTION_STATUS_ENUM;
 
   @ManyToOne(() => User)
   @Field(() => User)
